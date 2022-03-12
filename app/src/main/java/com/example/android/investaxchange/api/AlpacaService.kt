b1c95ec95e0ab010f1678dc557e415117f4b5bae
Andrew Dassonville <dassonville.andrew@gmail.com>
2022-03-11 17:21:09 -0800

    Fix API issues

--- app/src/main/java/com/example/android/investaxchange/api/AlpacaService.kt
+++ app/src/main/java/com/example/android/investaxchange/api/AlpacaService.kt
@@ -5,24 +5,31 @@ import com.example.android.investaxchange.data.UserAccountResults
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
-import retrofit2.http.GET
-import retrofit2.http.Query
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
 
-const val KEYID = BuildConfig.KEYID
-const val SECRETID = BuildConfig.SECRETID
-// TODO: replace with alpaca-java library
+val KEYID = BuildConfig.KEYID
+val SECRETID = BuildConfig.SECRETID
+
 interface AlpacaService {
     @GET("v2/account")
     suspend fun getAccount() : UserAccountResults
 
     companion object {
         private const val BASE_URL = "https://paper-api.alpaca.markets/"
+
         fun create() : AlpacaService {
             val client = OkHttpClient.Builder()
-                .addInterceptor(OAuthInterceptor(KEYID, SECRETID))
+                .addInterceptor { chain ->
+                    val request = chain.request().newBuilder()
+                        .addHeader("APCA-API-KEY-ID", KEYID)
+                        .addHeader("APCA-API-SECRET-KEY", SECRETID)
+                        .build()
+
+                    chain.proceed(request)
+                }
                 .build()
             val moshi = Moshi.Builder()
                 .addLast(KotlinJsonAdapterFactory())
