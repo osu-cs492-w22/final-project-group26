07efdcca7f67a5b7451c12c362666ecff0e51bbe
Andrew Dassonville <dassonville.andrew@gmail.com>
2022-03-16 14:03:40 -0700

    Improve buy order pricing

--- app/src/main/java/com/example/android/investaxchange/ui/MarketDetailFragment.kt
+++ app/src/main/java/com/example/android/investaxchange/ui/MarketDetailFragment.kt
@@ -121,15 +121,24 @@ class MarketDetailFragment : Fragment(R.layout.market_detail) {
         buy50Button.setOnClickListener { orderViewModel.createOrder(getOrderRequest(50)) }
     }
 
-    private fun getOrderRequest(qty: Int) = OrderRequest(
-        symbol = args.asset.symbol,
-        qty = qty,
-        side = "buy",
-        type = "limit",
-        limitPrice = snapshotViewModel.snapshot.value?.latestTrade?.price?.times(1.03),
-        timeInForce = "day",
-        extendedHours = true,
-    )
+    private fun getOrderRequest(qty: Int): OrderRequest {
+        val basePrice = snapshotViewModel.snapshot.value?.latestTrade?.price ?: 0.0
+        val limitPrice = basePrice * when {
+            basePrice <= 25.00 -> 1.1
+            basePrice <= 50.00 -> 1.05
+            else -> 1.03
+        }
+
+        return OrderRequest(
+            symbol = args.asset.symbol,
+            qty = qty,
+            side = "buy",
+            type = "limit",
+            limitPrice = limitPrice,
+            timeInForce = "day",
+            extendedHours = true,
+        )
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
         menuInflater.inflate(R.menu.activity_market_detail, menu)
